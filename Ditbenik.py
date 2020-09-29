88feb6b495a73ba3428b19729007d79f8adae126
sam449 <sam.bdebeer@gmail.com>
2020-09-29 20:03:08 +0200

    nieuw

--- Ditbenik.py
+++ Ditbenik.py
@@ -1,3 +1,5 @@
+from Question import Question
+
 question prompts = [
     "Waar kom ik vandaan?\n(a) amsterdam\n(b) diemen\n(c) Amstelveen\n\n",
     "Hoe reis ik naar school?\n(a) bus\n(b) tram\n(c) metro\n\n",
@@ -9,3 +11,13 @@ questions = [
     Question(question_prompts[0], "a"),
     Question(question_prompts[3], "c"),
 ]
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("You got" + str(score) + "/" + str(len(questions)) + "correct")
+
+run_test(questions)
