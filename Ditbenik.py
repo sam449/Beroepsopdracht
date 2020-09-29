5cef25cb3be32b7dcdea4dd6b1d356d0b50ce645
sam449 <sam.bdebeer@gmail.com>
2020-09-29 15:54:49 +0200

    nieuw

---
+++ Ditbenik.py
@@ -0,0 +1,11 @@
+question prompts = [
+    "Waar kom ik vandaan?\n(a) amsterdam\n(b) diemen\n(c) Amstelveen\n\n",
+    "Hoe reis ik naar school?\n(a) bus\n(b) tram\n(c) metro\n\n",
+    "Hoelang reis ik naar school?\n(a) 15min\n(b) 30min\n(c) 45min\n\n"
+]
+
+questions = [
+    Question(question_prompts[3], "c"),
+    Question(question_prompts[0], "a"),
+    Question(question_prompts[3], "c"),
+]
---
+++ Helloim.py
@@ -0,0 +1,4 @@
+print("Hallo ik ben sam")
+print("wie ben jij?")
+naam= input("Wat is je naam?")
+print("Hello" + naam)
